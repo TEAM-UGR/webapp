60917165555f9b8048bdd63424629ca14521680b
rohanbiradar54 <144705934+rohanbiradar22@users.noreply.github.com>
2024-02-07 22:56:08 -0500

    Assignment 2

---
+++ config/createDB.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2/promise');
+const dbConfig = require('./dbConfig');
+
+const createDatabase = async () => {
+  try {
+    const connection = await mysql.createConnection({
+      host: dbConfig.host,
+      user: dbConfig.user,
+      password: dbConfig.password,
+    });
+
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${dbConfig.database}\`;`);
+    console.log(`Database ${dbConfig.database} is ready.`);
+    await connection.end();
+  } catch (error) {
+    console.error('Unable to create database');
+    throw error; 
+  }
+};
+
+module.exports = createDatabase;
