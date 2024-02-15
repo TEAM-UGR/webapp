92008aa3e79063419e5e8974422f238e33b37963
rohanbiradar54 <144705934+rohanbiradar22@users.noreply.github.com>
2024-02-15 16:24:29 -0500

    Assignment 3 TEST

--- index.js
+++ index.js
@@ -76,11 +76,11 @@ app.use("/healthz", rejectAdditionalPathSegments(["/healthz"]));
 
 app.use(userAuthRouter);
 
-app.listen(3000, () => {
+app.listen(3000,() => {
   console.log("Application is running on http://localhost:3000");
   
 
-  createDatabaseAndSyncModels();
+    createDatabaseAndSyncModels()
 });
 
 async function createDatabaseAndSyncModels() {
