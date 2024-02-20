f96f6099e0773753563cd27c4b546a4ace6df619
rohanbiradar54 <144705934+rohanbiradar22@users.noreply.github.com>
2024-02-19 21:42:04 -0500

    Try 1

--- integration.test.js
+++ integration.test.js
@@ -20,7 +20,7 @@ describe("Integration Test 1 for creating a user and validating using GET", () =
       first_name: "Rohan",
       last_name: "Biradar",
       password: "Asb@1999",
-      username: "fgfgfg@mail.com",
+      username: "jjjjjj@mail.com",
     };
     const responsePOST = await request(app).post("/v1/user").send(req);
     expect(responsePOST.statusCode).toEqual(201);
@@ -37,7 +37,7 @@ describe("Integration Test 1 for creating a user and validating using GET", () =
 
 describe("Integration Test 2 for Updating a user and Validating using GET", () => {
   test("PUT /v1/user/self - success - Update User", async () => {
-    const username = "fgfgfg@mail.com";
+    const username = "jjjjjj@mail.com";
     const password = "Asb@1999";
     let token2 = `${username}:${password}`;
     let base64token2 = btoa(token2);
