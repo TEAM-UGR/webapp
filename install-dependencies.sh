ea31caa2fc4233fbbd3017e25bce4546dee0edca
rohanbiradar54 <144705934+rohanbiradar22@users.noreply.github.com>
2024-02-26 18:58:29 -0500

    Flo

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,6 +6,8 @@ sudo unzip /home/webapp-a2-main.zip -d /home
 
 sudo cp /tmp/start-web-app.service /etc/systemd/system
 
+sudo adduser csye6225 --shell /usr/sbin/nologin
+
 sudo chown -R csye6225:csye6225 /home/webapp-main
 
 sudo systemctl daemon-reload
