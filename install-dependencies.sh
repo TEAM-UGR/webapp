deeef6907ab3523a5ff6506502c7058820204316
rohanbiradar54 <144705934+rohanbiradar22@users.noreply.github.com>
2024-02-26 20:53:08 -0500

    G5

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,7 +2,7 @@
 set -e
 sudo cp /tmp/webapp-a2-main.zip /home
 echo "========================starting unzip============================================"
-sudo unzip /home/webapp-a2-main.zip -d /home
+sudo unzip /home/webapp-a2-main.zip -d /home/webapp-main
 echo "========================finishing unzip============================================"
 sudo cp /tmp/start-web-app.service /etc/systemd/system
 
