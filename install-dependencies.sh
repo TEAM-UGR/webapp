e4418e61ef1c3416a27afd16ecb2b71ea3a3f6c6
rohanbiradar54 <144705934+rohanbiradar22@users.noreply.github.com>
2024-02-26 19:50:10 -0500

    G1

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -8,6 +8,15 @@ sudo cp /tmp/start-web-app.service /etc/systemd/system
 
 sudo adduser csye6225 --shell /usr/sbin/nologin
 
+DIRECTORY="/home/webapp-main"
+
+if [ -d "$DIRECTORY" ]; then
+    echo "Listing contents of $DIRECTORY:"
+    ls -lart $DIRECTORY
+else
+    echo "Directory $DIRECTORY does not exist."
+fi
+
 sudo chown -R csye6225:csye6225 /home/webapp-main
 
 sudo systemctl daemon-reload
