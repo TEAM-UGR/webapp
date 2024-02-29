16ace2bd075b566e03617602d5ed0c0b4d379494
rohanbiradar54 <144705934+rohanbiradar22@users.noreply.github.com>
2024-02-28 19:31:57 -0500

    DB Fix 1

--- models/User.js
+++ models/User.js
@@ -1,52 +1,49 @@
 const { DataTypes } = require("sequelize");
-require('dotenv').config(); 
+require("dotenv").config();
 
-const {sequelize} = require("../config/db")
+const { sequelize } = require("../config/db");
 
-const User = sequelize.define('User',{
-    id:{
-        type: DataTypes.UUID,
-        primaryKey: true,
-        defaultValue: DataTypes.UUIDV4
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
     },
-        first_name:{
-            type: DataTypes.STRING,
-            allowNull: false,
-            field: 'first_name'
-        },
-        last_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            field: 'last_name' 
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            field: 'username'
-        }
-    }, {
-        tableName: 'users',
-        timestamps: true,
-        createdAt: "account_created",
-        updatedAt: "account_updated"
-    });
-    const syncModels = async()=>{
-        try{
-            await sequelize.sync({ force: false })
-       
-    
-        } catch(err){
-            console.log("Error while syncing")
-            
-        }
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: "first_name",
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: "last_name",
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: "username",
+    },
+  },
+  {
+    tableName: "users",
+    timestamps: true,
+    createdAt: "account_created",
+    updatedAt: "account_updated",
+  }
+);
+const syncModels = async () => {
+  try {
+    await sequelize.sync({ force: false });
+  } catch (err) {
+    console.log("Error while syncing");
+  }
+};
 
-    }
-    syncModels();
-    
-
-module.exports = User;
-
+module.exports = { User, syncModels };
