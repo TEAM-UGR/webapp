fd98ad7c07617dfa3d11fe31b5bc790379660b0a
rohanbiradar54 <144705934+rohanbiradar22@users.noreply.github.com>
2024-03-18 19:35:23 -0400

    Mapping severity

--- logging-setup.sh
+++ logging-setup.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -e
 
+echo "======================= Create folder structure for log file ================================"
 sudo mkdir -p /var/log/webapp-main
 
+echo "======================= Create log file ================================"
 sudo touch /var/log/webapp-main/webapp.log
 
+echo "======================= Copy config file to /tmp ================================"
 sudo cp /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
 
+echo "======================= Change ownership of log file ================================"
 sudo chown -R csye6225:csye6225 /var/log/webapp-main/webapp.log
 
+echo "======================= Restart Ops agent ================================"
 sudo systemctl restart google-cloud-ops-agent
