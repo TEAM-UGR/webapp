abad98b37c4c706458d4d8bc4574f1bfea751e76
rohanbiradar54 <144705934+rohanbiradar22@users.noreply.github.com>
2024-03-18 16:05:24 -0400

    Updating Ownership of log file

--- logging-setup.sh
+++ logging-setup.sh
@@ -9,4 +9,6 @@ sudo cp /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
 
 sudo chown -R csye6225:csye6225 /etc/google-cloud-ops-agent/config.yaml
 
+sudo chown -R csye6225:csye6225 /var/log/webapp-main/webapp.log
+
 sudo systemctl restart google-cloud-ops-agent
