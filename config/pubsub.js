cc8974a0cb3517eb0157df2ed0c9687cde11d89b
rohanbiradar54 <144705934+rohanbiradar22@users.noreply.github.com>
2024-03-26 20:58:37 -0400

    Adding loggers

--- config/pubsub.js
+++ config/pubsub.js
@@ -1,27 +1,31 @@
 const { PubSub } = require("@google-cloud/pubsub");
+const logger = require("../config/logger");
 
-const pub = async  (message,projectId, topicNameOrId, subscriptionName) => {
-    const pubsub = new PubSub({ projectId });
-    const topic = pubsub.topic(topicNameOrId);
-    const subscription = pubsub.subscription(subscriptionName);
-    subscription.on("message", (message) => {
-      console.log("1 Received message:", message.data.toString());
-      // console.log(topicNameOrId)
-      // console.log(subscriptionName)
-      // console.log(pubsub)
-    });
-    subscription.on("error", (error) => {
-      console.error("Received error:", error);
-    });
-    try {
-      topic.publishMessage({ data: Buffer.from(message) });
-      console.log("3 Received message:");
-      
-    } catch (error) {
-      console.log("Sommme error occurred")
-    }
+const pub = async (message, projectId, topicNameOrId, subscriptionName) => {
+  const pubsub = new PubSub({ projectId });
+  const topic = pubsub.topic(topicNameOrId);
+  const subscription = pubsub.subscription(subscriptionName);
+  subscription.on("message", (message) => {
+    console.log("1 Received message:", message.data.toString());
+    // console.log(topicNameOrId)
+    // console.log(subscriptionName)
+    // console.log(pubsub)
+  });
+  subscription.on("error", (error) => {
+    console.error("Received error:", error);
+    logger.error("Receiver error in pub sub", error);
+  });
+  try {
     topic.publishMessage({ data: Buffer.from(message) });
     
+    console.log("in try block pubsub.js:");
+  } catch (error) {
+    // console.log("Sommme error occurred");
+    console.error("Received error:", error);
   }
-  
-  module.exports = pub;
+  topic.publishMessage({ data: Buffer.from(message) });
+  console.log("send message to topic");
+  logger.info("Sent message to topic")
+};
+
+module.exports = pub;
