cba1cf1ff812259813bf1c963969f54e27cfa915
rohanbiradar54 <144705934+rohanbiradar22@users.noreply.github.com>
2024-03-27 11:15:23 -0400

    Update to pub sub.js

--- config/pubsub.js
+++ config/pubsub.js
@@ -5,25 +5,26 @@ const pub = async (message, projectId, topicNameOrId, subscriptionName) => {
   const pubsub = new PubSub({ projectId });
   const topic = pubsub.topic(topicNameOrId);
   const subscription = pubsub.subscription(subscriptionName);
-  subscription.on("message", (message) => {
-    console.log("1 Received message:", message.data.toString());
-    // console.log(topicNameOrId)
-    // console.log(subscriptionName)
-  });
-  subscription.on("error", (error) => {
-    console.error("Received error:", error);
-    logger.error("Receiver error in pub sub", error);
-  });
-  try {
-    topic.publishMessage({ data: Buffer.from(message) });
-    console.log("in try block pubsub.js:");
-  } catch (error) {
-    // console.log("Sommme error occurred");
-    console.error("Received error:", error);
-  }
+  // subscription.on("message", (message) => {
+  //   console.log("1 Received message:", message.data.toString());
+  //   console.log(topicNameOrId)
+  //   console.log(subscriptionName)
+  // });
+  // subscription.on("error", (error) => {
+  //   console.error("Received error:", error);
+  //   logger.error("Receiver error in pub sub", error);
+  // });
+  // try {
+  //   topic.publishMessage({ data: Buffer.from(message) });
+  //   console.log("in try block pubsub.js:");
+  // } catch (error) {
+  //   // console.log("Some error occurred");
+  //   console.error("Received error:", error);
+  // }
+  console.log("Sending message to topic",message);
   logger.info("Sending message to topic",message)
   topic.publishMessage({ data: Buffer.from(message) });
-  console.log("send message to topic",message);
+  console.log("sent message to topic",message);
   logger.info("Sent message to topic")
 };
 
