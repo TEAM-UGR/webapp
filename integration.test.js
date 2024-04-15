7a2aa93caeeaf11f0cb874d818e94047b90d005f
rohanbiradar54 <144705934+rohanbiradar22@users.noreply.github.com>
2024-04-15 19:33:45 -0400

    Demoo

--- integration.test.js
+++ integration.test.js
@@ -10,20 +10,20 @@ beforeAll(async () => {
 });
 
 describe("Integration Test 1 for creating a user and validating using GET", () => {
-  test("POST /v1/user - success - Create User", async () => {
+  test("POST /v2/user - success - Create User", async () => {
     const req = {
       first_name: "Rohan",
       last_name: "Biradar",
       password: "Asb@1999",
       username: "123456@mail.com",
     };
-    const responsePOST = await request(app).post("/v1/user").send(req);
+    const responsePOST = await request(app).post("/v2/user").send(req);
     expect(responsePOST.statusCode).toEqual(201);
     let token = `${req.username}:${req.password}`;
     let base64token = btoa(token);
 
     const responseCreateGET = await request(app)
-      .get("/v1/user/self")
+      .get("/v2/user/self")
       .send(req)
       .set("Authorization", `Basic ${base64token}`);
     expect(responseCreateGET.statusCode).toEqual(200);
@@ -33,7 +33,7 @@ describe("Integration Test 1 for creating a user and validating using GET", () =
 
 
 describe("Integration Test 2 for Updating a user and Validating using GET", () => {
-  test("PUT /v1/user/self - success - Update User", async () => {
+  test("PUT /v2/user/self - success - Update User", async () => {
     const username = "123456@mail.com";
     const password = "Asb@1999";
     let token2 = `${username}:${password}`;
@@ -45,7 +45,7 @@ describe("Integration Test 2 for Updating a user and Validating using GET", () =
       password: "9423523985",
     };
     const responsePUT = await request(app)
-      .put("/v1/user/self")
+      .put("/v2/user/self")
       .set("Authorization", `Basic ${base64token2}`)
       .send(reqUpdate);
 
@@ -55,7 +55,7 @@ describe("Integration Test 2 for Updating a user and Validating using GET", () =
     let base64token3 = btoa(token3);
 
     const responseUpdateGET = await request(app)
-      .get("/v1/user/self")
+      .get("/v2/user/self")
       .send()
       .set("Authorization", `Basic ${base64token3}`);
     expect(responseUpdateGET.statusCode).toEqual(200);
